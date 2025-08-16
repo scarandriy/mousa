f7e23a6f0e81165bf0c7faebc8a7e75c934ce88c
admin <scarandry@gmail.com>
2025-08-16 04:45:07 +0200

    fixed linting error

--- app/cars/[slug]/error.tsx
+++ app/cars/[slug]/error.tsx
@@ -1,9 +1,15 @@
 'use client';
-  export default function Error({ error, reset }) {
-    return (
-      <div>
-        <h2>Something went wrong!</h2>
-        <button onClick={reset}>Try again</button>
-      </div>
-    );
-  }
+
+interface ErrorProps {
+  error: Error & { digest?: string };
+  reset: () => void;
+}
+
+export default function Error({ error, reset }: ErrorProps) {
+  return (
+    <div>
+      <h2>Something went wrong!</h2>
+      <button onClick={reset}>Try again</button>
+    </div>
+  );
+}
